f7ed922924a932e2961e0a02434179e349e4ff4e
fhipfinger <fhipfinger@student.tgm.ac.at>
2023-09-13 09:17:36 +0200

    initial commit

---
+++ app/src/main/java/at/ac/tgm/hit/sew7/fhipfinger/rechner/MainActivity.java
@@ -0,0 +1,14 @@
+package at.ac.tgm.hit.sew7.fhipfinger.rechner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
